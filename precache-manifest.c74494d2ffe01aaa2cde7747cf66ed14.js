470efb48c1777fd49fa8b33272f3b459074a92f1
Ballonek <dominik.micek@seznam.cz>
2020-05-21 18:49:39 +0200

    Updates

---
+++ precache-manifest.c74494d2ffe01aaa2cde7747cf66ed14.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eff6c4ff0f36f4b6a02e98f6ce2bc0d",
+    "url": "/Portfolio-ReactJS-Frontend/index.html"
+  },
+  {
+    "revision": "b0aade7b9d9fed44ffd5",
+    "url": "/Portfolio-ReactJS-Frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4ddafd0cd3c2ea39e7c9",
+    "url": "/Portfolio-ReactJS-Frontend/static/css/main.061db600.chunk.css"
+  },
+  {
+    "revision": "b0aade7b9d9fed44ffd5",
+    "url": "/Portfolio-ReactJS-Frontend/static/js/2.a03d7c15.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Portfolio-ReactJS-Frontend/static/js/2.a03d7c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddafd0cd3c2ea39e7c9",
+    "url": "/Portfolio-ReactJS-Frontend/static/js/main.9b22748b.chunk.js"
+  },
+  {
+    "revision": "d180f015d8a589f8ca84",
+    "url": "/Portfolio-ReactJS-Frontend/static/js/runtime-main.194dd637.js"
+  },
+  {
+    "revision": "c040b7f365f5159acd05caf58da8470a",
+    "url": "/Portfolio-ReactJS-Frontend/static/media/DKBB.c040b7f3.PNG"
+  },
+  {
+    "revision": "207a54f0aa7de028a75036c0d66819c9",
+    "url": "/Portfolio-ReactJS-Frontend/static/media/Domin.207a54f0.PNG"
+  },
+  {
+    "revision": "f0573a26ee2ede47a972f8af9d659091",
+    "url": "/Portfolio-ReactJS-Frontend/static/media/DoppioOne-Regular.f0573a26.ttf"
+  },
+  {
+    "revision": "c8dda1ffb4f960fbadd06b27ac9a2d4b",
+    "url": "/Portfolio-ReactJS-Frontend/static/media/HC.c8dda1ff.PNG"
+  },
+  {
+    "revision": "5368dac5c38d1c1aaf57d67f47873a38",
+    "url": "/Portfolio-ReactJS-Frontend/static/media/Steelcom.5368dac5.PNG"
+  },
+  {
+    "revision": "4f3d3cf70752f6bca2a20f3102b4584a",
+    "url": "/Portfolio-ReactJS-Frontend/static/media/contactbg.4f3d3cf7.jpg"
+  },
+  {
+    "revision": "bc202eea2a28b30bd115fa2a6bd8f063",
+    "url": "/Portfolio-ReactJS-Frontend/static/media/topbg.bc202eea.jpg"
+  }
+]);
